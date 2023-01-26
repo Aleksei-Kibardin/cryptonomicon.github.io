3a0f229902c20f12aa071f9a32259d09d67dcfc7
Aleksei-Kibardin <96148536+Aleksei-Kibardin@users.noreply.github.com>
2023-01-26 13:07:09 +0300

    add taliwind

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
 import "./app.css";
 import App from "./App.vue";
+import "./assets/tailwind.css";
+import "./assets/tailwind.css";
 
 createApp(App).mount("#app");
