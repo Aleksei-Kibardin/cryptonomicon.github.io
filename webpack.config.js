c346a7612b79f9f4d345e9e359e6d21ef0bb4a3d
alex <kibardin2000@mail.ru>
2023-02-04 21:24:04 +0300

    test

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "./src/index.html",
+      template: "./public/index.html",
     }),
     new VueLoaderPlugin(),
   ],
