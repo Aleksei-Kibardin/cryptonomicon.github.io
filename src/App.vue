60b736694b2fdc4b70503bc02bcbd0acdf466acb
alex <96148536+Aleksei-Kibardin@users.noreply.github.com>
2023-02-26 17:29:58 +0300

    filteredTickers

--- src/App.vue
+++ src/App.vue
@@ -103,6 +103,8 @@
           </button>
           <button
             class="mx-2 my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+            @click="page = page + 1"
+            v-if="hasNextPage"
           >
             Вперед
           </button>
@@ -226,8 +228,30 @@ export default {
   },
 
   methods: {
-    filteredlist() {
-      return this.tickers.filter((ticker) => ticker.name.includes(this.filter));
+    filteredTickers() {
+      const start = (this.page - 1) * 6;
+      const end = this.page * 6 - 1;
+
+      const filteredTickers = this.tickers.filter((ticker) =>
+        ticker.name.includes(this.filter)
+      );
+
+      this.hasNextPage = filteredTickers.length > end;
+
+      return filteredTickers.slice(start, end);
+    },
+
+    filteredList() {
+      const start = (this.page - 1) * 6;
+      const end = this.page * 6 - 1;
+
+      const filteredlist = this.tickers.filter((ticker) =>
+        ticker.name.includes(this.filter)
+      );
+
+      this.hasNextPage = filteredlist.length > end;
+
+      return filteredlist.slice(start, end);
     },
 
     subscribeToUpdates(tickerName) {
