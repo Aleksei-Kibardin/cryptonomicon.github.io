71eb8334f5f4444bf0c59a5e5aa4863115536766
alex <96148536+Aleksei-Kibardin@users.noreply.github.com>
2023-02-26 16:02:42 +0300

    update pagination

--- src/App.vue
+++ src/App.vue
@@ -96,11 +96,14 @@
         <div>
           <button
             class="mx-2 my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+            v-if="page > 1"
+            @click="page = page - 1"
           >
             Назад
           </button>
           <button
             class="mx-2 my-4 inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+            @click="page = page + 1"
           >
             Вперед
           </button>
@@ -204,9 +207,11 @@ export default {
       tickers: [],
       sel: null,
       graph: [],
+      page: 1,
       active: false,
       hint: [],
       filter: "",
+      hasNextPage: true,
     };
   },
 
@@ -222,8 +227,17 @@ export default {
   },
 
   methods: {
-    filteredlist() {
-      return this.tickers.filter((ticker) => ticker.name.includes(this.filter));
+    filteredList() {
+      const start = (this.page - 1) * 6;
+      const end = this.page * 6 - 1;
+
+      const filteredlist = this.tickers.filter((ticker) =>
+        ticker.name.includes(this.filter)
+      );
+
+      this.hasNextPage = filteredlist.length > end;
+
+      return filteredlist.slice(start, end);
     },
 
     subscribeToUpdates(tickerName) {
@@ -264,6 +278,7 @@ export default {
     },
 
     handleDelete(tickerToRemove) {
+      localStorage.getItem("cryptonomicon-list");
       this.tickers = this.tickers.filter((t) => t !== tickerToRemove);
       clearInterval();
     },
